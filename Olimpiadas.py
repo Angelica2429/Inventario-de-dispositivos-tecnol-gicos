1d97b78fdc4d0709c7733be222085dfb8032c01c
Angélica2429 <benavidescabezas.angelica@gmail.com>
2025-06-19 17:38:46 -0500

    Ejercicio 2

---
+++ Olimpiadas.py
@@ -0,0 +1,67 @@
+print("Ejercicio # 2")
+
+usuarios= ["Camilo","Sara","Luisa"]
+dispositivos=["Laptop","Tablet","Smartphone"]
+
+#punto 1
+if "Luisa" in usuarios[2]:
+    usuarios.append("David")
+print(usuarios)
+
+#punto 2
+if "Smartphone" in dispositivos[2]:
+    dispositivos.append("Smartwatch")
+print(dispositivos)
+
+#punto 3
+if "Sara" in usuarios[1]:
+    usuarios.remove("Sara")
+print(usuarios)
+
+elem=len(dispositivos)
+print(elem)
+# #punto 4
+# if dispositivos.len==3:
+#     dispositivos.remote[0]
+# print(dispositivos)
+
+# #punto 5
+# if "Camilo" in usuarios[0]:
+#     usuarios.remove("Camilo")
+#     usuarios.append("Andres")
+# print(usuarios)
+
+# #punto 6
+# soporte_tecnico=["Luisa","David"]
+
+# #punto 7
+# dispositivos_moviles=["Smartphone","Smartwatch"]
+
+# #punto 8
+# if "Smartwatch" in dispositivos_moviles[1]:
+#     nuevo_dispositivo=("Smartwatch","disponible")
+# print(nuevo_dispositivo)
+
+# #punto 12
+# if "Impresora" in dispositivos:
+#     print("Ya está en la lista")
+# else:
+#     dispositivos.append("Impresora")
+# print(dispositivos)
+
+# #punto 13
+# if "Sara" in usuarios:
+#     print("Ya está en la lista")
+# else:
+#     usuarios.append("Sara")
+# print(usuarios)
+
+# print(dispositivos)
+
+# print(soporte_tecnico)
+
+# print(dispositivos_moviles)
+
+# print(nuevo_dispositivo)
+
+# print(registro_usuario)
